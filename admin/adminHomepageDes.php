b5c7419cc76bd52bc95ae795fda3afd76a1ede09
eabiii <edbar_10@yahoo.com>
2017-11-14 21:28:45 +0800

    Added a notification link(Not sure if correct)

--- admin/adminHomepageDes.php
+++ admin/adminHomepageDes.php
@@ -1,5 +1,17 @@
 <?php
 	include 'admincontent/adminheader.php';
+		$date=date('Y-m-d');
+		$sql="SELECT * FROM `vehicles` WHERE ownerId='". $userid."'";
+		$result=mysqli_query($con,$sql);
+		$row=mysqli_fetch_assoc($result);
+		$sql2="SELECT * FROM `basic vehicle inspection` 
+		INNER JOIN  `servicechecklist` ON `basic vehicle inspection`.basicInspectionID = `servicechecklist`.basicInspectionID
+		INNER JOIN  `services` ON `services`.serviceID = `servicechecklist`.serviceID
+		WHERE  `servicechecklist`.status='on progress' 
+		AND `services`.Type='Maintenance'";
+		$result2=mysqli_query($con,$sql2);
+	
+		// echo date( "Y-m-d", (strtotime($date) + 259200));
 ?>
 	<div class="main">	
 		<center>
@@ -41,12 +53,23 @@
 					</h5>					
 					<a href="adminChangeUsername.php"><h5>Change Username</h5></a>
 					<a href="adminChangePassword.php"><h5>Change Password</h5></a>
+                      <?php
+						$num = 0;
+						while($row2=mysqli_fetch_assoc($result2) ){
+                        $num = $num +1;
+                        }
+        if($num>0){
+            echo "<a href='adminCheckConfirmations.php'><h5>You have inread notifications!!!</h5></a>";
+        }
+					?>
 				</center>
 		</center>
 
 		<form class="myform" action="adminHomepageDes.php" method="POST">
 			<center><input name="logout" type="submit" id="logout_btn" value="Log Out"/></center>
 		</form>
+        
+      
 
 		<?php
 				if(isset($_POST['logout'])){
